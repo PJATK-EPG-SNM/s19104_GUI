ecd136350732df113a2ce15cfb8fe04e3c3edc02
s19104 <61684594+s19104@users.noreply.github.com>
2020-05-07 11:02:03 +0200

    01

---
+++ GUI/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Text gameOverText;
+    private bool gameOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                PlayerPrefs.SetInt(ScorePrefs.SCORE, 0);
+                PlayerPrefs.SetInt(ScorePrefs.PREVIOUS_SCORE, 0);
+                SceneManager.LoadScene("Menu");
+            }
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "GAME OVER - press 1 to reset, press 2 to go to the MENU";
+        gameOver = true;
+        PlayerPrefs.SetInt(ScorePrefs.SCORE, PlayerPrefs.GetInt(ScorePrefs.PREVIOUS_SCORE));
+    }
+}
---
+++ GUI/Assets/Scripts/Zbierable/Coin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour{
+
+    //private Animator anim;
+    private ScoreManager scoreManager;
+    private bool collected;
+    private int points = 1;
+
+    void Start(){
+        //anim = GetComponent<Animator>();
+        scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+        collected = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Player") && !collected) {
+            collected = true;
+            scoreManager.IncrementScore(points);
+           // anim.SetTrigger("collected");
+            Invoke("CollectCoin", 1.5f);
+        }
+    }
+
+    private void CollectCoin()
+    {
+        Destroy(gameObject);
+    }
+
+}
